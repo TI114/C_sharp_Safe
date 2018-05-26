51b73f6a583fd63cba9b4247c53b1e59c61bf3e8
unknown <marcel_reuter@arcor.de>
2018-05-26 14:27:17 +0200

    add buttons to Control for Creation/Open/Close

--- safe/Control.cs
+++ safe/Control.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace safe
 {
-    class Control
+    public partial class Control : Form
     {
+        public Control()
+        {
+            InitializeComponent();
+        }
+
+        Safe safe;
+
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            if (TbPassword != null)
+            {
+                safe = new Safe(TbPassword.Text);
+                BtnCreate.Enabled = false;
+            }
+            else
+            {
+                safe = new Safe();
+            }
+
+        }
+
+        private void BtnCloseOpen_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
