083ab23e61750252503840ae342f1168bfaca83d
unknown <marcel_reuter@arcor.de>
2018-05-26 18:43:00 +0200

    add update method

--- safe/Form1.cs
+++ safe/Form1.cs
@@ -8,13 +8,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace safe
+namespace safeNamespace
 {
     public partial class Form1 : Form, IObserver
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Update(IState state)
+        {
+            if (state == safeNamespace.Close.GetInstance())
+            {
+                this.BackColor = Color.Magenta;
+            }
+            else
+            {
+                this.BackColor = Color.LimeGreen;
+            }
         }
     }
 }
