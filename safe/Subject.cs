cc6c1a0ac0e5a025fbbb6f4ac2c02111c4c2226b
unknown <marcel_reuter@arcor.de>
2018-05-26 12:49:06 +0200

    implement subcject

--- safe/Subject.cs
+++ safe/Subject.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace safe
 {
-    class Subject
+    public abstract class Subject
     {
+        List<IObserver> observerList = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            observerList.Add(observer);
+            Notify();
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observerList.Remove(observer);
+
+        }
+
+        public void Notify()
+        {
+            foreach (IObserver observer in observerList)
+            {
+                observer.Update();
+            }
+        }
     }
 }
