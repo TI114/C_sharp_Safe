3fe930d7b127c796ed967f5827fd18f51fdc2670
unknown <marcel_reuter@arcor.de>
2018-05-26 18:43:29 +0200

    add picture for state of safe

--- safe/Form2.cs
+++ safe/Form2.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
-namespace safe
+namespace safeNamespace
 {
     public partial class Form2 : Form, IObserver
     {
         public Form2()
         {
             InitializeComponent();
+        }
+
+        public void LoadPictureOpen()
+        {
+            string PfadBild = @"./../../Open_Safe.png";
+            StreamReader SR = new StreamReader(PfadBild);
+            Bitmap Bild = new Bitmap(SR.BaseStream);
+            SR.Close();
+            this.pictureBox1.Size = new System.Drawing.Size(Bild.Size.Width, Bild.Size.Height);
+            this.pictureBox1.Image = Bild;
+        }
+
+        public void LoadPictureClose()
+        {
+            string PfadBild = @"./../../Closed_Safe.png";
+            StreamReader SR = new StreamReader(PfadBild);
+            Bitmap Bild = new Bitmap(SR.BaseStream);
+            SR.Close();
+            this.pictureBox1.Size = new System.Drawing.Size(Bild.Size.Width, Bild.Size.Height);
+            this.pictureBox1.Image = Bild;
+        }
+
+        public void Update(IState state)
+        {
+            if (state == safeNamespace.Close.GetInstance())
+            {
+                LoadPictureClose();
+            }
+            else
+            {
+                LoadPictureOpen();
+            }
+
+
         }
     }
 }
