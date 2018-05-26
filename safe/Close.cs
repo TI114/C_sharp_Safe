8db88df827b3a62084e603024d0bc71674ec48d0
unknown <marcel_reuter@arcor.de>
2018-05-26 13:08:15 +0200

    add close state

--- safe/Close.cs
+++ safe/Close.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace safe
 {
-    class Close
+    class Close : IState
     {
+        private static IState status;
+
+        private Close()
+        {
+
+        }
+
+        public static IState GetInstance()
+        {
+            if (status == null)
+            {
+                status = new Close();
+            }
+            return status;
+        }
+
+        public void safeClose(Safe safe)
+        {
+            // stays empty
+        }
+
+        public void safeOpen(Safe safe)
+        {
+            // todo: implement
+        }
     }
 }
