50ad6828acc4d4a6b0a7b08e00a1e47ba8b1e575
unknown <marcel_reuter@arcor.de>
2018-05-26 13:36:06 +0200

    implement Safe

--- safe/Safe.cs
+++ safe/Safe.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace safe
 {
-    class Safe
+    public class Safe : Subject
     {
+        private string password = "geheim-1";
+
+        private IState status;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public Safe()
+        {
+            status.SafeClose(this);
+        }
+
+        public Safe (String password)
+        {
+            if (ValidatePassword(password))
+            {
+                Password = password;
+            }
+
+            status.SafeClose(this);
+        }
+
+        private Boolean ValidatePassword(String password)
+        {
+            if (password.Length >= 8 && password.Any(char.IsDigit) && password.Any(char.IsLetter) && Regex.IsMatch(password, @"\W"))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
